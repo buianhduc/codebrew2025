e90875cf05aa4b73ff3b0a721e2124fc9faca77f
sheepamania <shawn.perera123d@gmail.com>
2025-04-25 11:26:50 +1000

    Base rendering code

---
+++ src/main.c
@@ -0,0 +1,75 @@
+#include "pixel_grid.h"
+#include "triangles.h"
+#include <stdbool.h>
+
+Tigr* window;
+Tigr* gScreen;
+
+// Where to draw the red pixel
+int red_pixel_x = 240, red_pixel_y = 200;
+
+triangle_t tri;
+
+vec3_t camera_position;
+vec3_t camera_rotation;
+
+// Runs 60 times per second
+// Can be used for timers, simulation state updates, etc.
+void tick_state() {
+	if (tigrKeyHeld(window, TK_DOWN)){
+		camera_position.z += 0.01;
+	}
+}
+
+void render_state() {
+
+	
+	set_view_matrix(&camera_position, 0, 0);
+
+
+	render_triangle(&tri);
+}
+
+void initialize(){
+	camera_position = vec3(0, 0, 0);
+	camera_rotation = vec3(0, 0, 0);
+
+	initialize_matrices();
+
+	vec3_t p1 = vec3(0.0, 1.0 , -2.0);
+	vec3_t p2 = vec3(1.0, -1.0 , -2.0);
+	vec3_t p3 = vec3(-1.0, -1.0 , -2.0);
+
+	vertex_t v1 = make_basic_vertex(&p1);
+	vertex_t v2 = make_basic_vertex(&p2);
+	vertex_t v3 = make_basic_vertex(&p3);
+
+	tri = make_triangle(&v1, &v2, &v3);
+}
+
+int main(int argc, char* argv[]) {
+	// Create window
+	window = tigrWindow(PIXEL_GRID_WIDTH, PIXEL_GRID_HEIGHT, "Software Renderer", TIGR_FIXED);
+
+	//initialize shit 
+	initialize();
+	
+
+	// Create externally-accessible 320x240 bitmap
+	gScreen = tigrBitmap(PIXEL_GRID_WIDTH, PIXEL_GRID_HEIGHT);
+
+    while (!tigrClosed(window) && !tigrKeyDown(window, TK_ESCAPE)) {
+        tigrClear(gScreen, tigrRGB(0, 0, 0));
+
+		tick_state();
+		render_state();
+
+		tigrBlit(window, gScreen, 0, 0, 0, 0, PIXEL_GRID_WIDTH, PIXEL_GRID_HEIGHT);
+
+        tigrUpdate(window);
+    }
+
+    tigrFree(window);
+
+	return 0;
+}
---
+++ src/triangles.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "triangles.h"
+#include "tigr.h"
+#include "pixel_grid.h"
+
+
+mat4x4_t projection_matrix;
+mat4x4_t view_matrix; 
+
+void initialize_matrices() {
+    double a = 320/240.0;
+    double fov = 70.0 * M_PI/180;
+    double z_near = 0.01;
+    double z_far = 100;
+    projection_matrix = make_projection_matrix(a, fov, z_near, z_far);
+
+    load_identity(&view_matrix);
+}
+
+void draw_point(vec4_t *p){
+    // if (p->z <= 0) {
+    //     return;
+    // }
+
+    int pixel_x = p->x / p->z * PIXEL_GRID_HEIGHT / 2 + PIXEL_GRID_WIDTH / 2;
+    int pixel_y = p->y / p->z * PIXEL_GRID_HEIGHT / 2 + PIXEL_GRID_HEIGHT / 2;
+
+    const int square_size = 8;
+    tigrFill(gScreen, pixel_x - square_size / 2, pixel_y - square_size / 2, square_size, square_size, tigrRGB(255, 0, 255));
+    tigrPlot(gScreen, pixel_x, pixel_y, tigrRGB(255, 255, 0));
+}
+
+void render_triangle(triangle_t *tri){
+
+    //here
+
+    //get position of the vertices on screen 
+    vec4_t p1 = mat4x4_multiply(&projection_matrix,
+       tri->vertices[0].position);
+    vec4_t p2 = mat4x4_multiply(&projection_matrix,
+        tri->vertices[1].position);
+    vec4_t p3 = mat4x4_multiply(&projection_matrix,
+        tri->vertices[2].position);
+
+    draw_point(&p1);
+    draw_point(&p2);
+    draw_point(&p3);
+
+}
+
+void set_view_matrix(vec3_t * p, double x_rotation, double y_rotation) {
+    mat4x4_t translate = make_translation_matrix(vec3(
+        -p->x,
+        -p->y,
+        -p->z
+    ));
+    mat4x4_t x_rotate = make_rotation_matrix_x(x_rotation);
+    mat4x4_t y_rotate = make_rotation_matrix_x(y_rotation);
+
+    //view_matrix = y_rotate * x_rotate * translate (write multiply matrices method)
+}
+
