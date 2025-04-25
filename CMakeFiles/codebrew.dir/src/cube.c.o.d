9e17150b843a67e61f5d9eed7c96e6f1d0583344
Duc Bui <32723856+buianhduc@users.noreply.github.com>
2025-04-25 11:57:41 +1000

    clean up

--- CMakeFiles/codebrew.dir/src/cube.c.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/codebrew.dir/src/cube.c.o: \
-  /Users/ducbui/Documents/Coding/codebrew/src/cube.c
--- CMakeFiles/codebrew.dir/src/ray.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/codebrew.dir/src/ray.c.o: \
-  /Users/ducbui/Documents/Coding/codebrew/src/ray.c \
-  /Users/ducbui/Documents/Coding/codebrew/src/ray.h \
-  /Users/ducbui/Documents/Coding/codebrew/src/triangles.h
